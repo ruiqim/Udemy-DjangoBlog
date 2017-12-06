50fcb571b25a50a7778437e82fae1b7e042522e3
rmao4 <rmao4@ford.com>
2017-12-06 16:08:28 -0500

    added bootstrap

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -6,9 +6,11 @@ from .models import Post
 
 def home(request):
 
-    posts = Post.objects.order_by('pub_date')
+    posts = Post.objects.order_by('-pub_date')
 
     return render(request, 'posts/home.html',{'posts':posts})
 
 def post_details(request, post_id):
-    return render(request, 'posts/post_detail.html', {'post_id':post_id})
+
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post':post})
