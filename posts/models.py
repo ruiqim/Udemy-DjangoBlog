9b80efd6ac066958418958022c6278481eb937fe
rmao4 <rmao4@ford.com>
2017-12-06 10:17:04 -0500

    added homepage display with backend data

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
