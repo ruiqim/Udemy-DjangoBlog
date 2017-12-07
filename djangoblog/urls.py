62c33c7c2842d458d0b629787f8c39840c143d9e
rmao4 <rmao4@ford.com>
2017-12-07 16:34:37 -0500

    added formatting to post page

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from posts import views
+import posts.views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details,name="post_detail"),
+    url(r'^$', posts.views.home, name="home"),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', posts.views.post_details,name="post_detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
